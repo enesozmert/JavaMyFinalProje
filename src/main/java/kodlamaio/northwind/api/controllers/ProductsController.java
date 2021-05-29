69c368143af7d1eff2e1521b0eddc2bc16dedff6
enesozmert <en-es111@hotmail.com>
2021-05-29 20:38:28 +0300

    29.05.2021 hafta 8 crud operation added

--- src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
+++ src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import kodlamaio.northwind.business.abstracts.ProductService;
@@ -17,23 +18,50 @@ import kodlamaio.northwind.entities.concretes.Product;
 @RestController
 @RequestMapping("/api/products")
 public class ProductsController {
-	
+
 	private ProductService productService;
-	
+
 	@Autowired
 	public ProductsController(ProductService productService) {
 		super();
 		this.productService = productService;
 	}
 
-	@GetMapping("/getall")	
+	@GetMapping("/getAll")
 	public DataResult<List<Product>> getAll() {
 		return this.productService.getAll();
 	}
+
+	@GetMapping("/getAllByPage")
+	public DataResult<List<Product>> getAll(int pageNo, int pageSize) {
+		return this.productService.getAll(pageNo, pageSize);
+	}
 	
+	@GetMapping("/getAllDesc")
+	public DataResult<List<Product>> getAllSorted() {
+		return this.productService.getAllSorted();
+	}
+
 	@PostMapping("/add")
 	public Result add(@RequestBody Product product) {
 		return this.productService.add(product);
 	}
+
+	@GetMapping("/getByProductName")
+	public DataResult<Product> GetByProductName(@RequestParam String productName) {
+		return this.productService.getByProductName(productName);
+	}
+
+	@GetMapping("/getByProductNameAndCategoryId")
+	public DataResult<Product> getByProductNameAndCategoryId(@RequestParam("productName") String productName,
+			@RequestParam("categoryId") int categoryId) {
+		return this.productService.getByProductNameAndCategoryId(productName, categoryId);
+	}
+
+	@GetMapping("/getByProductNameContains")
+	public DataResult<List<Product>> getByProductNameContains(@RequestParam("productName") String productName) {
+		return this.productService.getByProductNameContains(productName);
+	}
+
 }
 //kodlama.io/api/products
